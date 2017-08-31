07727be00705dabe43b993fbb3180c3dcb59c71b
Fan Hongxiang (NIGLAS) <fanhongxiang13@mails.ucas.ac.cn>
2017-08-31 14:25:43 +0800

    modify format_rfggrid

--- R/format_rfggrid.R
+++ R/format_rfggrid.R
@@ -1,18 +1,35 @@
 #' Format Delft3D RFGGRID (.grd) file.
 #' @param src_grd Character. Full name of the Delft3D RFGGRID (.grd) file to be formatted.
 #' @param dst_grd Character. Full name of the formatted Delft3D RFGGRID (.grd) file.
+#' @param na_pattern Character. The MISSING VALUE in the src_grd.
+#' @param na_repalcement Character. A character vector of replacements.
+#' @param coord_sys Character. Coordinate system in the grd.
 #' @export
-format_rfggrid <- function(src_grd, dst_grd)
+
+format_rfggrid <- function(src_grd,
+                           dst_grd,
+                           na_pattern = "-9.99999000000000024E\\+02",
+                           na_replacement = "0.00000000000000000E+00",
+                           coord_sys = "Cartesian")
 {
-  require(stringr)
-  frgfile <- file(src_grd, "r")
-  rfg.in <- readLines(frgfile)
-  close(frgfile)
-  missing.val <- stringr::str_split_fixed(rfg.in[6], " ", 12)[12]
-  missing.val.split <- stringr::str_split_fixed(missing.val, "\\+", 2)
-  missing.val.pattern <- paste0(missing.val.split[1], "\\+", missing.val.split[2])
-  rfg.out <- file(dst_grd, "a")
-  for (i in 1:5) writeLines(rfg.in[i], rfg.out)
-  for (i in 7:length(rfg.in)) writeLines(do.call(paste, as.list(stringr::str_replace(str_split(rfg.in[i], " ")[[1]], pattern = missing.val.pattern, "0"))), rfg.out)
-  close(rfg.out, "rw")
+
+  src_file <- file(src_grd, "r")
+  src_char <- readLines(src_file)
+  close(src_file)
+  if(file.exists(dst_grd)){unlink(dst_grd)}
+  dst_file <- file(dst_grd, "a")
+  writeLines("* DS-International EFDC_Explorer Generated", dst_file)
+  writeLines("* WL | Delft Hydraulics, Delft3D-RGFGRID Version 4.12.02.20; Apr 2005", dst_file)
+  writeLines(paste("* File creation date:", format(Sys.time(), "%F %T")), dst_file)
+  writeLines("*", dst_file)
+  writeLines(paste("Coordinate System =", coord_sys), dst_file)
+  writeLines(paste("Missing Value =", na_replacement), dst_file)
+  for (i in 7:length(src_char)) writeLines(do.call(paste, as.list(stringr::str_replace(stringr::str_split(src_char[i], " ")[[1]],
+                                                                                       pattern = na_pattern,
+                                                                                       replacement = na_replacement)
+                                                                  )
+                                                   ),
+                                           dst_file)
+  close(dst_file, "rw")
 }
+
