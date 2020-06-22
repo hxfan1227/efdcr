7db58e5737b37f9d83194309c6831205826f9b57
Fan Hongxiang <fanhongxiang13@mails.ucas.ac.cn>
2020-06-22 14:34:30 +0800

    update docs

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' Pipe operator
+#'
+#' Pipe an object forward into a function or call expression. 
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
